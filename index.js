b77a1b7e1131f0b25344a4c01611f0a3dbdabf59
Eric Kunzel <kunzel.eric@gmail.com>
2023-05-22 14:34:22 -0300

    Project setup + packages installed

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({data: 'teste123'})
+})
 
 app.listen(8080, () => {
     console.log('Server running on port 8080.')
